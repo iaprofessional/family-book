43cb79fce2b708394ff0eb82d5c1173789f4bce2
iaprofessional <75013595+iaprofessional@users.noreply.github.com>
2021-02-03 18:38:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var images = ["https://images-na.ssl-images-amazon.com/images/I/91zbi9M+mKL.jpg","https://i.pinimg.com/736x/d2/de/95/d2de9556e9eef282d01f208bfb8d5090.jpg", "https://cdn.24.co.za/files/Cms/General/d/5115/072489d36fe54f7691e30e7fe564d55a.png", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+var images = ["https://images-na.ssl-images-amazon.com/images/I/91zbi9M+mKL.jpg", "https://i.pinimg.com/736x/d2/de/95/d2de9556e9eef282d01f208bfb8d5090.jpg", "https://cdn.24.co.za/files/Cms/General/d/5115/072489d36fe54f7691e30e7fe564d55a.png", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
 var names = ["Fmaily Book","Rayhan Harshad", "Abdul Harshad", "Ashly Harshad"];
 var i = 0;
 function update()
